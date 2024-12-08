20df1b3ff07a5f001997782522fbd749fafe5255
MalicknND <ndiayemalick062@gmail.com>
2024-12-08 18:09:24 +0100

    Semantic structure

---
+++ src/components/Main.js
@@ -0,0 +1,19 @@
+function Main() {
+  return (
+    <main>
+      <section>
+        <h1>Welcome to Little Lemon</h1>
+        <p>Your favorite place to enjoy delicious meals.</p>
+      </section>
+      <section>
+        <h2>Our Specials</h2>
+        <ul>
+          <li>Spaghetti Carbonara</li>
+          <li>Grilled Salmon</li>
+          <li>Vegetarian Pizza</li>
+        </ul>
+      </section>
+    </main>
+  );
+}
+export default Main;
